d71d423b58b50a70cf1edccdce84f4edf079f896
Leo Naab <Lnaab@ucsd.edu>
2023-01-25 15:52:48 -0800

    fixed reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,21 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedLong(){
+    int[] input1 = {4, 7, 9};
+    int[] expectedOut = {9, 7, 4};
+
+    assertArrayEquals(expectedOut, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlaceLong(){
+    int[] input1 = {4, 8, 3, 9};
+    int[] expectedOut = {9, 3, 8, 4};
+    ArrayExamples.reverseInPlace(input1);
+
+    assertArrayEquals(expectedOut, input1);
+  }
 }
