4ae96c7ea5c925a9d2be7b0f08d9627a321fd1fc
Leo Naab <Lnaab@ucsd.edu>
2023-01-25 16:30:25 -0800

    tested average

--- ArrayTests.java
+++ ArrayTests.java
@@ -32,4 +32,23 @@ public class ArrayTests {
 
     assertArrayEquals(expectedOut, input1);
   }
+
+  @Test
+  public void testReverseInPlaceLongest(){
+    int[] input1 = {4, 8, 3, 9, 7, 4, 5, 7};
+    int[] expectedOut = {7, 5, 4, 7, 9, 3, 8, 4};
+    ArrayExamples.reverseInPlace(input1);
+
+    assertArrayEquals(expectedOut, input1);
+  }
+
+  @Test
+  public void testAverageWithout(){ 
+    double[] theNums = {1, 2, 3, 4, 5};
+    double theAns = ArrayExamples.averageWithoutLowest(theNums);
+    String theWarning = String.format("Got %f", theAns);
+    assertEquals(theWarning,(double)(2 + 3 + 4 + 5)/4.0, theAns, 0.001);
+  }
+
+  
 }
